20a0f8e662e9da9ac3e907d8205dc2336e6b0de5
realvaleh <90547690+realvaleh@users.noreply.github.com>
2026-09-16 22:35:29 -0400

    Hotfix Swift 6 / Xcode-beta Release build
    Match the working Mac binary: PreferenceKey defaultValue as let, and assign notch geometry in IslandModule.init without calling applyScreen before stored properties are set.

--- Shore/Design/ShoreTheme.swift
+++ Shore/Design/ShoreTheme.swift
@@ -366,7 +366,7 @@ struct ShoreMarquee: View {
 }
 
 private struct MarqueeWidthKey: PreferenceKey {
-    static var defaultValue: CGFloat = 0
+    static let defaultValue: CGFloat = 0
     static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
         value = max(value, nextValue())
     }
